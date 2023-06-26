a27599e97f584c44fa6facad62f11ae9d2520d24
Melvine Nargeot <melvine.nargeot@protonmail.com>
2023-06-26 21:32:49 +0200

    Fix course fields index

--- src/components/FieldList/FieldList.tsx
+++ src/components/FieldList/FieldList.tsx
@@ -1,24 +1,18 @@
 import React from 'react'
-import {useDocsSidebar} from "@docusaurus/theme-common/internal";
-import GithubSlugger from 'github-slugger';
+import Link from '@docusaurus/Link';
+import Translate from '@docusaurus/Translate';
 
 export default function FieldList() {
-    const sidebar = useDocsSidebar();
-    const githubSlugger = new GithubSlugger();
     return (
         <article className="margin-top--lg">
             <section className="row">
-                {sidebar.items.map(item => {
-                    if (item.type === 'category') {
-                        return (
-                            <article className="field-item col col--6 margin-bottom--lg">
-                                <a className="card padding--lg" href={"/docs/category/" + githubSlugger.slug(item.label, {maintainCase: true})}>
-                                    <h2 className="text--truncate" title="Vector spaces">📄️ {item.label}</h2>
-                                </a>
-                            </article>
-                        )
-                    }
-                })}
+                <article className="field-item col col--6 margin-bottom--lg">
+                    <Link className="card padding--lg" to="/docs/category/algebra">
+                        <h2 title="Algebra">
+                            <Translate id="fields.algebra" />
+                        </h2>
+                    </Link>
+                </article>
             </section>
         </article>
     );
